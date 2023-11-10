a159ef2f4147ac9880fc862abd2eee8a4b937125
valerii.korniienko <vakor.dev@gmail.com>
2023-11-10 19:43:41 +0200

    +Service Locator +TextAnimator

---
+++ Assets/_Project/Scripts/ServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Project.Scripts
+{
+    public static class ServiceLocator
+    {
+        private static Dictionary<Type, IService> _services = new();
+        public static T GetService<T>() where T : IService
+        {
+            return (T) _services[typeof(T)];
+        }
+        
+        public static void RegisterService<T>(T service) where T : IService
+        {
+            _services.Add(typeof(T), service);
+        }
+        
+        public static void UnregisterService<T>() where T : IService
+        {
+            _services.Remove(typeof(T));
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/TextAnimator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using KBCore.Refs;
+using MEC;
+using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class TextAnimator : MonoBehaviour
+    {
+        [SerializeField, Self] private TextMeshProUGUI text;
+        [SerializeField] private float animationSpeed = 0.2f;
+
+        private string _finalText;
+        private int _currentCharIndex;
+        private char[] _chars;
+        private bool _isPaused;
+        private CoroutineHandle _animationCoroutine;
+
+
+        private void OnValidate()
+        {
+            this.ValidateRefs();
+        }
+
+        private void Awake()
+        {
+            _finalText = text.text;
+            _chars = _finalText.ToCharArray();
+        }
+
+        public void StartAnimation()
+        {
+            _animationCoroutine = Timing.RunCoroutine(AnimateTextCoroutine());
+            _isPaused = false;
+        }
+
+        private IEnumerator<float> AnimateTextCoroutine()
+        {
+            while (_currentCharIndex < _chars.Length)
+            {
+                text.text += _chars[_currentCharIndex].ToString();
+                _currentCharIndex++;
+                yield return Timing.WaitForSeconds(animationSpeed);
+            }
+        }
+
+        public void PauseAnimation()
+        {
+            if (!_isPaused)
+            {
+                Timing.PauseCoroutines(_animationCoroutine);
+                _isPaused = true;
+            }else
+            {
+                Timing.ResumeCoroutines(_animationCoroutine);
+                _isPaused = false;
+            }
+        }
+
+        public void Reset()
+        {
+            text.text = "";
+            _currentCharIndex = 0;
+        }
+
+        public void FinishAnimation()
+        {
+            Timing.KillCoroutines(_animationCoroutine);
+            text.text = _finalText;
+            _isPaused = false;
+        }
+    }
+}
