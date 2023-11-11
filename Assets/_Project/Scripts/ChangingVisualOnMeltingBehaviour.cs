b1610bc9ec8cb8144497cf43795ee4aa72ef634b
valerii.korniienko <vakor.dev@gmail.com>
2023-11-11 12:06:08 +0200

    Torch + IBehaviour

---
+++ Assets/_Project/Scripts/ChangingVisualOnMeltingBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class ChangingVisualOnMeltingBehaviour : IBehaviour
+    {
+        private MeltingObject _meltingObject;
+        private List<FreezingVisuals> _freezingStages;
+        private Transform _transform;
+
+        private GameObject _defaultVisualsPrefab;
+        private GameObject _currentVisuals;
+
+        public ChangingVisualOnMeltingBehaviour(
+            List<FreezingVisuals> freezingStages,
+            MeltingObject meltingObject, 
+            Transform transform)
+        {
+            _freezingStages = freezingStages;
+            _meltingObject = meltingObject;
+            _transform = transform;
+        }
+
+        public void OnEnable()
+        {
+            _meltingObject.OnFreezingChanged += OnFreezingChanged;
+        }
+
+        public void Awake()
+        {
+            if (_freezingStages.Count != 0)
+            {
+                _defaultVisualsPrefab = _freezingStages.First(v => v.maxFreezingBound == 1).frozenVisualsPrefab;
+                _freezingStages = _freezingStages.OrderBy(v => v.maxFreezingBound).ToList();
+            }
+
+            ChangeVisuals(_defaultVisualsPrefab);
+        }
+
+        private void OnFreezingChanged(float currentFreezing)
+        {
+            foreach (var freezingStage in _freezingStages)
+            {
+                if (currentFreezing <= freezingStage.maxFreezingBound)
+                {
+                    ChangeVisuals(freezingStage.frozenVisualsPrefab);
+                    return;
+                }
+            }
+        }
+
+        private void ChangeVisuals(GameObject newVisualsPrefab)
+        {
+            if (_currentVisuals != null)
+            {
+                Object.Destroy(_currentVisuals);
+            }
+
+            _currentVisuals = Object.Instantiate(newVisualsPrefab, _transform);
+        }
+
+        public void OnDisable()
+        {
+            _meltingObject.OnFreezingChanged -= OnFreezingChanged;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Torch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using KBCore.Refs;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class Torch : ActivationMechanism
+    {
+        [SerializeField, Self] private MeltingObject meltingObject;
+        [SerializeField] private List<FreezingVisuals> freezingStages;
+        [SerializeField] private GameObject visualPlaceholder;
+
+        private IBehaviour _meltingBehaviour;
+        
+        public event Action OnTorchLit;
+
+        private void OnValidate()
+        {
+            this.ValidateRefs();
+        }
+
+        private void Awake()
+        {
+            visualPlaceholder.SetActive(false);
+            _meltingBehaviour =
+                new ChangingVisualOnMeltingBehaviour(freezingStages, meltingObject, transform);
+            _meltingBehaviour.Awake();
+        }
+
+        private void OnEnable()
+        {
+            meltingObject.OnTotallyMelted += LightTorch;
+            _meltingBehaviour.OnEnable();
+        }
+
+        private void LightTorch()
+        {
+            OnTorchLit?.Invoke();
+        }
+
+        private void OnDisable()
+        {
+            meltingObject.OnTotallyMelted -= LightTorch;
+            _meltingBehaviour.OnDisable();
+        }
+    }
+}
