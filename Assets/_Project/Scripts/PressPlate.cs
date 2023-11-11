21e43c3cf77531cf3d95af1fad929a37344e41b7
valerii.korniienko <vakor.dev@gmail.com>
2023-11-11 15:02:23 +0200

    New assets

--- Assets/_Project/Scripts/PressPlate.cs
+++ Assets/_Project/Scripts/PressPlate.cs
@@ -6,16 +6,25 @@ namespace _Project.Scripts
     public class PressPlate : ActivationMechanism
     {
         [SerializeField, Self] private Collider2D plateCollider;
+        [SerializeField] private GameObject pressedVisual;
+        [SerializeField] private GameObject unpressedVisual;
         
         private void OnValidate()
         {
             this.ValidateRefs();
+        }
+        
+        private void Awake()
+        {
+            pressedVisual.SetActive(false);
+            unpressedVisual.SetActive(true);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.GetComponent<Player>())
             {
+                SetVisualsPressed(true);
                 InvokeOnPlatePressed();
             }
         }
@@ -24,8 +33,15 @@ namespace _Project.Scripts
         {
             if (other.gameObject.GetComponent<Player>())
             {
+                SetVisualsPressed(false);
                 InvokeOnPlatePressed();
             }
+        }
+        
+        private void SetVisualsPressed(bool pressed)
+        {
+            pressedVisual.SetActive(pressed);
+            unpressedVisual.SetActive(!pressed);
         }
     }
 }
